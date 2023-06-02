21ba353869f909108706f3d5887ba3e30791f7f3
Shehab <shehabembaby@gmail.com>
2023-06-03 01:40:52 +0200

    LJ-1: notes | JS Delete function added

--- app.js
+++ app.js
@@ -2,16 +2,34 @@ console.log("HyperMove Taxi App");
 
 function saveNote() {
     var note = document.getElementById("noteInput").value;
-    localStorage.setItem("userNote", note);
+    var savedNotes = JSON.parse(localStorage.getItem("userNotes")) || [];
+    savedNotes.push(note);
+    localStorage.setItem("userNotes", JSON.stringify(savedNotes));
     displayNotes();
     alert("Note saved successfully!");
 }
 
+function deleteNote(index) {
+    var savedNotes = JSON.parse(localStorage.getItem("userNotes")) || [];
+    savedNotes.splice(index, 1);
+    localStorage.setItem("userNotes", JSON.stringify(savedNotes));
+    displayNotes();
+}
+
 function displayNotes() {
-    var savedNote = localStorage.getItem("userNote");
+    var savedNotes = JSON.parse(localStorage.getItem("userNotes")) || [];
     var notesBox = document.getElementById("notesBox");
-    if (savedNote) {
-        notesBox.textContent = savedNote;
+
+    if (savedNotes.length > 0) {
+        notesBox.innerHTML = ""; // Clear existing notes
+
+        for (var i = 0; i < savedNotes.length; i++) {
+            var note = savedNotes[i];
+            var noteElement = document.createElement("div");
+            noteElement.classList.add("note");
+            noteElement.innerHTML = "<span>" + note + "</span><i class='delete-icon' onclick='deleteNote(" + i + ")'>&times;</i>";
+            notesBox.appendChild(noteElement);
+        }
     } else {
         notesBox.textContent = "No notes saved.";
     }
