6b5c0d9717313bd0dcfe144327633eecb048800e
Shehab <shehabembaby@gmail.com>
2023-06-03 01:30:06 +0200

    LJ-1: First JS function to save notes

--- app.js
+++ app.js
@@ -1,1 +1,22 @@
 console.log("HyperMove Taxi App");
+
+function saveNote() {
+    var note = document.getElementById("noteInput").value;
+    localStorage.setItem("userNote", note);
+    displayNotes();
+    alert("Note saved successfully!");
+}
+
+function displayNotes() {
+    var savedNote = localStorage.getItem("userNote");
+    var notesBox = document.getElementById("notesBox");
+    if (savedNote) {
+        notesBox.textContent = savedNote;
+    } else {
+        notesBox.textContent = "No notes saved.";
+    }
+}
+
+window.onload = function() {
+    displayNotes();
+}
